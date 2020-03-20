f359e5f81b93083048dcc719ca37806c84ce6085
fragStark <958602243@qq.com>
2020-03-20 17:30:02 +0800

    update user name

--- flutter_app/lib/like.dart
+++ flutter_app/lib/like.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
-import 'package:flutterapp/photo.dart';
 
 class LikeApp extends StatelessWidget {
   @override
