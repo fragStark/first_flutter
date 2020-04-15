a70cc87c7ff0918321263b11fa5abca38e23f086
fragStark <958602243@qq.com>
2020-04-15 18:24:08 +0800

    udpate fragment

---
+++ shoeShop/app/src/main/java/com/lemon/shoeshop/FragmentOne.kt
@@ -0,0 +1,20 @@
+package com.lemon.shoeshop
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class FragmentOne : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_one, container, false);
+        val view: TextView = root.findViewById(R.id.text);
+        return root;
+    }
+}
